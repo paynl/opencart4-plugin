53a3d780abe96ae3e65430d7d14b4818c406fdb7
woutse <wouter@pay.nl>
2025-07-16 09:32:54 +0200

    Code polish

--- admin/view/javascript/pay.js
+++ admin/view/javascript/pay.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
     $('.payPaymentMethod').each(function () {
         $(this).click(function (e) {
-            if ($(e.target).hasClass('clickable')) {            
+            if ($(e.target).hasClass('clickable')) {
                 if (!$(this).hasClass('open')) {
                     $('.payPaymentMethod').removeClass('open')
                     $(this).addClass('open')
@@ -28,7 +28,7 @@ $(document).ready(function () {
         e.preventDefault();
         var ajaxurl = $('#pay_checkversion_url').val();
         var data = {
-            'versionCheck' : $('#pay_current_version').val()
+            'versionCheck': $('#pay_current_version').val()
         };
         $('#paynl_version_check_result').hide();
         $('#paynl_version_check_loading').css('display', 'block');
@@ -76,10 +76,10 @@ $(document).ready(function () {
         var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]/i
         if ($.trim(message) == '' || ($.trim(email) != '' && !regex.test($('#suggestions_form_email').val()))) {
             if ($.trim(email) != '' && !regex.test($('#suggestions_form_email').val())) {
-            $('#email_error').css('display', 'inline')
+                $('#email_error').css('display', 'inline')
             }
             if ($.trim(message) == '') {
-            $('#message_error').css('display', 'inline')
+                $('#message_error').css('display', 'inline')
             }
             return false
         }
@@ -93,22 +93,22 @@ $(document).ready(function () {
         }
         setTimeout(function () {
             $.ajax({
-            url: ajaxurl,
-            type: 'POST',
-            data: data,
-            dataType: 'json',
-            success: function (data) {
-                if (data.success) {
-                $('#suggestions_form_email').val('')
-                $('#suggestions_form_message').val('')
-                $('#suggestions_form_success').modal('show')
-                } else {
-                $('#suggestions_form_fail').modal('show')
+                url: ajaxurl,
+                type: 'POST',
+                data: data,
+                dataType: 'json',
+                success: function (data) {
+                    if (data.success) {
+                        $('#suggestions_form_email').val('')
+                        $('#suggestions_form_message').val('')
+                        $('#suggestions_form_success').modal('show')
+                    } else {
+                        $('#suggestions_form_fail').modal('show')
+                    }
+                },
+                error: function () {
+                    $('#suggestions_form_fail').modal('show')
                 }
-            },
-            error: function () {
-                $('#suggestions_form_fail').modal('show')
-            }
             })
         }, 750)
     })
@@ -136,13 +136,13 @@ $(document).ready(function () {
     })
 
     $('.obscuredInput').each(function () {
-      var input = this;
-      $('<a class="obscuredDisplayShow"></a>').click(function () {
-        toggleObscured(input)
-      }).insertAfter(input);
+        var input = this;
+        $('<a class="obscuredDisplayShow"></a>').click(function () {
+            toggleObscured(input)
+        }).insertAfter(input);
     })
 })
 
-function toggleObscured (element) {
-  $(element).parent().find('.obscuredInput').toggleClass('display');
+function toggleObscured(element) {
+    $(element).parent().find('.obscuredInput').toggleClass('display');
 }
